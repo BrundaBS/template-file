540781bc6bfc5887674acbe136af5c8346a1635b
Brunda BS <kube-backup@example.com>
2019-01-11 15:47:09 +0530

    test chnages

---
+++ base-template-file/assets/template-files/scripts/test.sh
@@ -0,0 +1,19 @@
+gcloud compute instances create image-test --image=$IMAGE_NAME --subnet=default --zone=us-east4-c
+
+gcloud compute ssh image-test --zone us-east4-c --ssh-key-file=./base-template-file/assets/template-files/id_rsa
+
+/tmp/gossfolder/goss --version
+
+cd /tmp/gossfolder
+ 
+GOSS_FAIL_COUNT=/tmp/gossfolder/goss validate | grep "Failed:" | awk '{print substr($4,1,1)}'
+
+if [$GOSS_FAIL_COUNT==0]
+ then
+ echo "Goss test failed!!!"
+ gcloud compute images delete $IMAGE_NAME
+fi
+
+echo "Goss test passed"
+
+
