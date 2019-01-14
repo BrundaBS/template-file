879f1cbd5bd4bf2b5535a5e53ef40c62b72c88e2
Brunda BS <kube-backup@example.com>
2019-01-14 08:45:06 +0530

    removing unnecessary files

--- base-template-file/variable.tf
+++
@@ -1,5 +0,0 @@
-variable "compute_project" {
-  type        = "string"
-  description = "Project in which the compute resources need to be created"
-  default     = "gcp-ushi-platform-devops-npe"
-}
