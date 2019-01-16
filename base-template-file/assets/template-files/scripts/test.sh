7a47c66b41c3a92efc86c6c5dd6b23e121d9e4e6
Brunda BS <kube-backup@example.com>
2019-01-16 12:08:21 +0530

    change

--- base-template-file/assets/template-files/scripts/test.sh
+++ base-template-file/assets/template-files/scripts/test.sh
@@ -1,9 +1,9 @@
 curl -s "http://metadata.google.internal/computeMetadata/v1/project/attributes/jenkins_ssh_key" -H "Metadata-Flavor: Google" >> /var/lib/jenkins/ssh_key
 chown -R jenkins:jenkins /var/lib/jenkins/ssh_key
 
-gcloud compute instances create image-test --image=$IMAGE_NAME --subnet=default --zone=us-east4-c
+gcloud compute instances create image-test1 --image=$IMAGE_NAME --subnet=default --zone=us-east4-c
 
-gcloud compute ssh image-test --zone us-east4-c --ssh-key-file=/var/lib/jenkins/ssh_key
+gcloud compute ssh image-test1 --zone us-east4-c --ssh-key-file=/var/lib/jenkins/ssh_key
 
 /tmp/gossfolder/goss --version
 
