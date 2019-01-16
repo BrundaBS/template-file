3f05ff557ce8fd1ed8489f5929bb7fcecd7cb82a
Brunda BS <kube-backup@example.com>
2019-01-16 11:14:03 +0530

    changes

--- base-template-file/assets/template-files/scripts/install.sh
+++ base-template-file/assets/template-files/scripts/install.sh
@@ -4,9 +4,3 @@ sudo curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 
 sudo chmod 777 install-logging-agent.sh
 
-/tmp/gossfolder/goss --version
-
-cd /tmp/gossfolder
-
-/tmp/gossfolder/goss validate
-
