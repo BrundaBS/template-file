d30c2183a5b5629281aa51fae22c1f8b68eb8b39
Brunda BS <kube-backup@example.com>
2019-01-17 13:44:07 +0530

    change in test

--- base-template-file/assets/template-files/scripts/test.sh
+++ base-template-file/assets/template-files/scripts/test.sh
@@ -14,12 +14,3 @@ if [ $GOSS_FAIL_COUNT == 0 ]
  echo "Goss test passed!!!"
 fi
 
- export IMAGE_NAME=$(echo $FILE | rev | cut -d '.' -f2 | rev)-$(date +"%Y%m%d")
-         echo $IMAGE_NAME
-         /usr/local/packer build $updated_file
-        
-         chmod +x ./base-template-file/assets/template-files/scripts/test.sh
-         ./base-template-file/assets/template-files/scripts/test.sh
-         
-         echo "image name is"$IMAGE_NAME
-         gcloud compute images add-labels $IMAGE_NAME --labels=stable=$GIT_COMMIT
