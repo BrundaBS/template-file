57262b1fbd96e5ef8deabd1f397df2b3fd0c1336
Brunda BS <kube-backup@example.com>
2019-01-18 09:34:10 +0530

    test changes

--- base-template-file/assets/template-files/scripts/test.sh
+++ base-template-file/assets/template-files/scripts/test.sh
@@ -1,7 +1,6 @@
 
 echo $GIT_BRANCH
 
-
 gcloud compute instances create image-test --image=$IMAGE_NAME --subnet=default --zone=us-east4-c
 #gcloud compute instances create venkyblack123 --image=duplicate3 --subnet=default --zone=us-east4-c
  
@@ -11,11 +10,12 @@ export GOSS_FAIL_COUNT=$(echo $GOSS_RESULT | grep "Failed:" | awk '{print substr
 
 if [ $GOSS_FAIL_COUNT == "0" ]
  then
- echo "Goss test passed!!!"
- gcloud compute images add-labels $IMAGE_NAME --labels=stable=$GIT_COMMIT
- gcloud compute instances delete image-test --zone=us-east4-c --quiet
- exit 0
+  echo "Goss test passed!!!"
+  gcloud compute images add-labels $IMAGE_NAME --labels=stable=$GIT_COMMIT
+  gcloud compute instances delete image-test --zone=us-east4-c --quiet
+  exit 0
 fi
+
 echo "Goss test Failed!!!"
 gcloud compute images delete $IMAGE_NAME --quiet
 gcloud compute instances delete image-test --zone=us-east4-c --quiet
