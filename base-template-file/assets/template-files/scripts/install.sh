488ce9b965e2c4c43b1b084793f34d3acb29dcc0
Brunda BS <kube-backup@example.com>
2019-01-18 09:13:16 +0530

    add java

--- base-template-file/assets/template-files/scripts/install.sh
+++ base-template-file/assets/template-files/scripts/install.sh
@@ -4,3 +4,9 @@ sudo curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 
 sudo chmod 777 install-logging-agent.sh
 
+sudo yum update 
+
+sudo yum -y install java-1.8.0-openjdk-headless
+
+sudo yum -y install java-1.8.0-openjdk-devel
+
