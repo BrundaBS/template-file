4345ec06c51cb6ffd08bcc541954021debb742b7
Brunda BS <kube-backup@example.com>
2019-01-24 14:42:40 +0530

    changes

--- os-images/ubuntu/install.sh
+++ os-images/ubuntu/install.sh
@@ -4,9 +4,4 @@ sudo curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 
 sudo chmod 777 install-logging-agent.sh
 
-sudo yum update 
-
-sudo yum -y install java-1.8.0-openjdk-headless
-
-sudo yum -y install java-1.8.0-openjdk-devel
 
